2af80cf39acb3cb470478719512599a9e4f42117
leemyunggyu <jaylee@hatiolab.com>
2019-10-02 10:41:48 +0800

    v0.0.131

--- server/graphql/resolvers/worksheet/generate-arrival-notice-worksheet.ts
+++ server/graphql/resolvers/worksheet/generate-arrival-notice-worksheet.ts
@@ -11,6 +11,7 @@ import { getManager, getRepository } from 'typeorm'
 import { WORKSHEET_STATUS, WORKSHEET_TYPE } from '../../../constants'
 import { Worksheet, WorksheetDetail } from '../../../entities'
 import { WorksheetNoGenerator } from '../../../utils/worksheet-no-generator'
+import { Bizplace } from '@things-factory/biz-base'
 
 export const generateArrivalNoticeWorksheet = {
   async generateArrivalNoticeWorksheet(_: any, { arrivalNoticeNo, bufferLocation }, context: any) {
@@ -20,117 +21,110 @@ export const generateArrivalNoticeWorksheet = {
        *    - data existing
        *    - status of arrival notice
        */
-      const arrivalNotice: ArrivalNotice = await getRepository(ArrivalNotice).findOne({
-        where: { domain: context.state.domain, name: arrivalNoticeNo },
+      const foundArrivalNotice: ArrivalNotice = await getRepository(ArrivalNotice).findOne({
+        where: { domain: context.state.domain, name: arrivalNoticeNo, status: ORDER_STATUS.ARRIVED },
         relations: ['bizplace', 'orderProducts', 'orderVass']
       })
 
-      if (!arrivalNotice) throw new Error(`Arrival notice doesn't exists.`)
-      if (arrivalNotice.status !== ORDER_STATUS.ARRIVED)
-        throw new Error('Status is not suitable for getting ready to unload')
+      if (!foundArrivalNotice) throw new Error(`Arrival notice doesn't exists.`)
+      const customerBizplace: Bizplace = foundArrivalNotice.bizplace
+      let foundOPs: OrderProduct[] = foundArrivalNotice.orderProducts
+      let foundOVs: OrderVas[] = foundArrivalNotice.orderVas
 
+      if (!bufferLocation || !bufferLocation.id) throw new Error(`Can't find buffer location`)
+      const foundBufferLoc: Location = await getRepository(Location).findOne(bufferLocation.id)
+      if (!foundBufferLoc) throw new Error(`location doesn't exists.`)
       /*
        * 2. Create worksheet and worksheet details for products
        */
-      const orderProducts: [OrderProduct] = arrivalNotice.orderProducts
-      let unloadingWorksheet: Worksheet = new Worksheet()
-      if (orderProducts && orderProducts.length) {
-        unloadingWorksheet = await getRepository(Worksheet).save({
+      // 2. 1) Create unloading worksheet
+      const unloadingWorksheet = await getRepository(Worksheet).save({
+        domain: context.state.domain,
+        bizplace: customerBizplace,
+        name: WorksheetNoGenerator.unloading(),
+        bufferLocation: foundBufferLoc,
+        arrivalNotice: foundArrivalNotice,
+        type: WORKSHEET_TYPE.UNLOADING,
+        status: WORKSHEET_STATUS.DEACTIVATED,
+        creator: context.state.user,
+        updater: context.state.user
+      })
+
+      // 2. 2) Create unloading worksheet details
+      const unloadingWorksheetDetails = foundOPs.map((op: OrderProduct) => {
+        return {
           domain: context.state.domain,
-          bizplace: arrivalNotice.bizplace,
-          name: WorksheetNoGenerator.unloading(),
-          bufferLocation: await getRepository(Location).findOne(bufferLocation.id),
-          arrivalNotice: arrivalNotice,
+          bizplace: customerBizplace,
+          worksheet: unloadingWorksheet,
+          name: WorksheetNoGenerator.unloadingDetail(),
+          targetProduct: op,
           type: WORKSHEET_TYPE.UNLOADING,
           status: WORKSHEET_STATUS.DEACTIVATED,
           creator: context.state.user,
           updater: context.state.user
-        })
+        }
+      })
+      await getRepository(WorksheetDetail).save(unloadingWorksheetDetails)
 
-        await Promise.all(
-          orderProducts.map(async (orderProduct: OrderProduct) => {
-            await getRepository(WorksheetDetail).save({
-              domain: context.state.domain,
-              bizplace: arrivalNotice.bizplace,
-              worksheet: unloadingWorksheet,
-              name: WorksheetNoGenerator.unloadingDetail(),
-              targetProduct: orderProduct,
-              type: WORKSHEET_TYPE.UNLOADING,
-              status: WORKSHEET_STATUS.DEACTIVATED,
-              creator: context.state.user,
-              updater: context.state.user
-            })
-
-            // 3. 2) Update status of order products (ARRIVED => READY_TO_UNLOAD)
-            return await getRepository(OrderProduct).update(
-              {
-                domain: context.state.domain,
-                name: orderProduct.name,
-                arrivalNotice: arrivalNotice
-              },
-              {
-                ...orderProduct,
-                status: ORDER_PRODUCT_STATUS.READY_TO_UNLOAD,
-                updater: context.state.user
-              }
-            )
-          })
-        )
-      }
+      // 2. 3) Update status of order products (ARRIVED => READY_TO_UNLOAD)
+      foundOPs = foundOPs.map((op: OrderProduct) => {
+        return {
+          ...op,
+          status: ORDER_PRODUCT_STATUS.READY_TO_UNLOAD,
+          updater: context.state.user
+        }
+      })
+      await getRepository(OrderProduct).save(foundOPs)
 
       /**
-       * 4. Create worksheet detail for vass (if it exists)
+       * 3. Create worksheet and worksheet details for vass (if it exists)
        */
-      const orderVass: [OrderVas] = arrivalNotice.orderVass
       let vasWorksheet: Worksheet = new Worksheet()
-      if (orderVass && orderVass.length) {
+      if (foundOVs && foundOVs.length) {
+        // 2. 1) Create vas worksheet
         vasWorksheet = await getRepository(Worksheet).save({
           domain: context.state.domain,
-          bizplace: arrivalNotice.bizplace,
+          bizplace: customerBizplace,
           name: WorksheetNoGenerator.vas(),
-          arrivalNotice: arrivalNotice,
+          arrivalNotice: foundArrivalNotice,
           type: WORKSHEET_TYPE.VAS,
           status: WORKSHEET_STATUS.DEACTIVATED,
           creator: context.state.user,
           updater: context.state.user
         })
 
-        await Promise.all(
-          orderVass.map(async (orderVas: OrderVas) => {
-            await getRepository(WorksheetDetail).save({
-              domain: context.state.domain,
-              bizplace: arrivalNotice.bizplace,
-              worksheet: vasWorksheet,
-              name: WorksheetNoGenerator.vasDetail(),
-              targetVas: orderVas,
-              type: WORKSHEET_TYPE.VAS,
-              status: WORKSHEET_STATUS.DEACTIVATED,
-              creator: context.state.user,
-              updater: context.state.user
-            })
+        // 2. 2) Create vas worksheet details
+        const vasWorksheetDetails = foundOVs.map((ov: OrderVas) => {
+          return {
+            domain: context.state.domain,
+            bizplace: customerBizplace,
+            worksheet: vasWorksheet,
+            name: WorksheetNoGenerator.vasDetail(),
+            targetVas: ov,
+            type: WORKSHEET_TYPE.VAS,
+            status: WORKSHEET_STATUS.DEACTIVATED,
+            creator: context.state.user,
+            updater: context.state.user
+          }
+        })
+        await getRepository(WorksheetDetail).save(vasWorksheetDetails)
 
-            // 4. 2) Update status of order vass (ARRIVED => READY_TO_PROCESS)
-            await getRepository(OrderVas).update(
-              {
-                domain: context.state.domain,
-                name: orderVas.name,
-                arrivalNotice: arrivalNotice
-              },
-              {
-                ...orderVas,
-                status: ORDER_VAS_STATUS.READY_TO_PROCESS,
-                updater: context.state.user
-              }
-            )
-          })
-        )
+        // 2. 3) Update status of order products (ARRIVED => READY_TO_UNLOAD)
+        foundOVs = foundOVs.map((ov: OrderVas) => {
+          return {
+            ...ov,
+            status: ORDER_VAS_STATUS.READY_TO_PROCESS,
+            updater: context.state.user
+          }
+        })
+        await getRepository(OrderVas).save(foundOVs)
       }
 
       /**
        * 5. Update status of arrival notice (ARRIVED => READY_TO_UNLOAD)
        */
       await getRepository(ArrivalNotice).save({
-        ...arrivalNotice,
+        ...foundArrivalNotice,
         status: ORDER_STATUS.READY_TO_UNLOAD,
         updater: context.state.user
       })
