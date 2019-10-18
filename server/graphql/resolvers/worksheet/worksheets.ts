922222f7d5740c95b7602bcf339b3fb5971f543f
leemyunggyu <jaylee@hatiolab.com>
2019-10-18 19:50:02 +0900

    v0.0.196

--- server/graphql/resolvers/worksheet/worksheets.ts
+++ server/graphql/resolvers/worksheet/worksheets.ts
@@ -1,45 +1,49 @@
 import { Bizplace } from '@things-factory/biz-base'
 import { ArrivalNotice, ReleaseGood } from '@things-factory/sales-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository, In } from 'typeorm'
+import { getRepository, In, IsNull } from 'typeorm'
 import { Worksheet } from '../../../entities'
 
 export const worksheetsResolver = {
   async worksheets(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
 
-    const arrivalNoticeParam: any = params.find((param: any) => param.name === 'arrivalNoticeNo')
+    const arrivalNoticeParam: any = params.filters.find((param: any) => param.name === 'arrivalNoticeNo')
     if (arrivalNoticeParam) {
       const foundArrivalNotices: ArrivalNotice[] = await getRepository(ArrivalNotice).find({
         ...convertListParams({ filters: [{ ...arrivalNoticeParam, name: 'name' }] })
       })
       if (foundArrivalNotices && foundArrivalNotices.length) {
         convertedParams.where.arrivalNotice = In(foundArrivalNotices.map((foundAN: ArrivalNotice) => foundAN.id))
+      } else {
+        convertListParams.where.arrivalNotice = IsNull()
       }
     }
 
-    const releaseGoodParam = params.find(param => param.name === 'releaseGoodNo')
+    const releaseGoodParam = params.filters.find(param => param.name === 'releaseGoodNo')
     if (releaseGoodParam) {
       const foundReleaseGoods: ReleaseGood[] = await getRepository(ReleaseGood).find({
         ...convertListParams({ filters: [{ ...releaseGoodParam, name: 'name' }] })
       })
       if (foundReleaseGoods && foundReleaseGoods.length) {
         convertedParams.where.releaseGood = In(foundReleaseGoods.map((foundRG: ReleaseGood) => foundRG.id))
+      } else {
+        convertListParams.where.releaseGood = IsNull()
       }
     }
 
-    const bizplaceParam = params.find(param => param.name === 'bizplaceName')
+    const bizplaceParam = params.filters.find(param => param.name === 'bizplaceName')
     if (bizplaceParam) {
       const foundBizplaces: Bizplace[] = await getRepository(Bizplace).find({
         where: {
-          ...convertListParams({ filters: [{ ...bizplaceParam, name: 'name' }] }),
+          ...convertListParams({ filters: [{ ...bizplaceParam, name: 'name' }] }).where,
           bizplace: In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
         }
       })
       if (foundBizplaces && foundBizplaces.length) {
         convertedParams.where.bizplace = In(foundBizplaces.map((foundBizplace: Bizplace) => foundBizplace.id))
       } else {
-        convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+        convertedParams.where.bizplace = IsNull()
       }
     } else {
       convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
