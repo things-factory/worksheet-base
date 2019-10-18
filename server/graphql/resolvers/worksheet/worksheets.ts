0671aa1818126c82231a4a0c9199aab09dfb4fad
leemyunggyu <jaylee@hatiolab.com>
2019-10-18 19:32:07 +0900

    v0.0.195

--- server/graphql/resolvers/worksheet/worksheets.ts
+++ server/graphql/resolvers/worksheet/worksheets.ts
@@ -1,4 +1,5 @@
 import { Bizplace } from '@things-factory/biz-base'
+import { ArrivalNotice, ReleaseGood } from '@things-factory/sales-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
 import { getRepository, In } from 'typeorm'
 import { Worksheet } from '../../../entities'
@@ -6,7 +7,43 @@ import { Worksheet } from '../../../entities'
 export const worksheetsResolver = {
   async worksheets(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
-    convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+
+    const arrivalNoticeParam: any = params.find((param: any) => param.name === 'arrivalNoticeNo')
+    if (arrivalNoticeParam) {
+      const foundArrivalNotices: ArrivalNotice[] = await getRepository(ArrivalNotice).find({
+        ...convertListParams({ filters: [{ ...arrivalNoticeParam, name: 'name' }] })
+      })
+      if (foundArrivalNotices && foundArrivalNotices.length) {
+        convertedParams.where.arrivalNotice = In(foundArrivalNotices.map((foundAN: ArrivalNotice) => foundAN.id))
+      }
+    }
+
+    const releaseGoodParam = params.find(param => param.name === 'releaseGoodNo')
+    if (releaseGoodParam) {
+      const foundReleaseGoods: ReleaseGood[] = await getRepository(ReleaseGood).find({
+        ...convertListParams({ filters: [{ ...releaseGoodParam, name: 'name' }] })
+      })
+      if (foundReleaseGoods && foundReleaseGoods.length) {
+        convertedParams.where.releaseGood = In(foundReleaseGoods.map((foundRG: ReleaseGood) => foundRG.id))
+      }
+    }
+
+    const bizplaceParam = params.find(param => param.name === 'bizplaceName')
+    if (bizplaceParam) {
+      const foundBizplaces: Bizplace[] = await getRepository(Bizplace).find({
+        where: {
+          ...convertListParams({ filters: [{ ...bizplaceParam, name: 'name' }] }),
+          bizplace: In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+        }
+      })
+      if (foundBizplaces && foundBizplaces.length) {
+        convertedParams.where.bizplace = In(foundBizplaces.map((foundBizplace: Bizplace) => foundBizplace.id))
+      } else {
+        convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+      }
+    } else {
+      convertedParams.where.bizplace = In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+    }
 
     const [items, total] = await getRepository(Worksheet).findAndCount({
       ...convertedParams,
