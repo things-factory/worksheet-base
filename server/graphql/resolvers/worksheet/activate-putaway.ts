c20a92ba2d337a3cefa5bd99f8dc5840cb9d3c03
leemyunggyu <jaylee@hatiolab.com>
2019-11-12 18:00:39 +0900

    v0.9.3

--- server/graphql/resolvers/worksheet/activate-putaway.ts
+++ server/graphql/resolvers/worksheet/activate-putaway.ts
@@ -1,95 +1,87 @@
 import { Bizplace } from '@things-factory/biz-base'
 import { ArrivalNotice, OrderInventory, ORDER_PRODUCT_STATUS, ORDER_STATUS } from '@things-factory/sales-base'
-import { getManager, getRepository } from 'typeorm'
+import { getManager } from 'typeorm'
 import { WORKSHEET_STATUS } from '../../../constants'
 import { Worksheet, WorksheetDetail } from '../../../entities'
 
 export const services = {
   activatePutaway: async (
-    worksheetNo: string,
-    putawayWorksheetDetails: WorksheetDetail[],
-    context: any
-  ): Promise<Worksheet> => {
-    /**
-     * 1. Validation for worksheet
-     *    - data existing
-     *    - status of worksheet
-     */
-    const foundWorksheet: Worksheet = await getRepository(Worksheet).findOne({
-      where: {
-        domain: context.state.domain,
-        name: worksheetNo,
-        status: WORKSHEET_STATUS.DEACTIVATED
-      },
-      relations: ['bizplace', 'arrivalNotice', 'worksheetDetails', 'worksheetDetails.targetInventory']
-    })
-
-    if (!foundWorksheet) throw new Error(`Worksheet doesn't exists`)
-    const customerBizplace: Bizplace = foundWorksheet.bizplace
-    const foundWSDs: WorksheetDetail[] = foundWorksheet.worksheetDetails
-    let targetInventories: OrderInventory[] = foundWSDs.map((foundWSD: WorksheetDetail) => foundWSD.targetInventory)
-
-    /**
-     * 2. Update description of putaway worksheet details
-     */
-    await Promise.all(
-      putawayWorksheetDetails.map(async (putawayWorksheetDetail: WorksheetDetail) => {
-        await getRepository(WorksheetDetail).update(
-          {
-            domain: context.state.domain,
-            bizplace: customerBizplace,
-            name: putawayWorksheetDetail.name,
-            status: WORKSHEET_STATUS.DEACTIVATED
-          },
-          {
-            description: putawayWorksheetDetail.description,
-            status: WORKSHEET_STATUS.EXECUTING,
-            updater: context.state.user
-          }
-        )
-      })
-    )
-
-    /**
-     * 3. Update target inventories (status: READY_TO_PUTAWAY => PUTTING_AWAY)
-     */
-    targetInventories = targetInventories.map((targetInventory: OrderInventory) => {
-      return {
-        ...targetInventory,
-        status: ORDER_PRODUCT_STATUS.PUTTING_AWAY,
-        updater: context.state.user
-      }
-    })
-    await getRepository(OrderInventory).save(targetInventories)
-
-    /**
-     * 4. Update putaway Worksheet (status: DEACTIVATED => EXECUTING)
-     */
-    const worksheet: Worksheet = await getRepository(Worksheet).save({
-      ...foundWorksheet,
-      status: WORKSHEET_STATUS.EXECUTING,
-      startedAt: new Date(),
-      updater: context.state.user
-    })
-
-    /**
-     * 5. Update Arrival Notice (status: READY_TO_PUTAWAY => PUTTING_AWAY)
-     */
-    const arrivalNotice: ArrivalNotice = foundWorksheet.arrivalNotice
-    await getRepository(ArrivalNotice).save({
-      ...arrivalNotice,
-      status: ORDER_STATUS.PUTTING_AWAY,
-      updater: context.state.user
-    })
-
-    return worksheet
-  }
-}
-
 export const activatePutaway = {
   async activatePutaway(_: any, { worksheetNo, putawayWorksheetDetails }, context: any) {
-    return await getManager().transaction(async trxMgr =>
-      services.activatePutaway(worksheetNo, putawayWorksheetDetails, context)
-    )
+    return await getManager().transaction(async trxMgr => {
+      /**
+       * 1. Validation for worksheet
+       *    - data existing
+       *    - status of worksheet
+       */
+      const foundWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
+        where: {
+          domain: context.state.domain,
+          name: worksheetNo,
+          status: WORKSHEET_STATUS.DEACTIVATED
+        },
+        relations: ['bizplace', 'arrivalNotice', 'worksheetDetails', 'worksheetDetails.targetInventory']
+      })
+
+      if (!foundWorksheet) throw new Error(`Worksheet doesn't exists`)
+      const customerBizplace: Bizplace = foundWorksheet.bizplace
+      const foundWSDs: WorksheetDetail[] = foundWorksheet.worksheetDetails
+      let targetInventories: OrderInventory[] = foundWSDs.map((foundWSD: WorksheetDetail) => foundWSD.targetInventory)
+
+      /**
+       * 2. Update description of putaway worksheet details
+       */
+      await Promise.all(
+        putawayWorksheetDetails.map(async (putawayWorksheetDetail: WorksheetDetail) => {
+          await trxMgr.getRepository(WorksheetDetail).update(
+            {
+              domain: context.state.domain,
+              bizplace: customerBizplace,
+              name: putawayWorksheetDetail.name,
+              status: WORKSHEET_STATUS.DEACTIVATED
+            },
+            {
+              description: putawayWorksheetDetail.description,
+              status: WORKSHEET_STATUS.EXECUTING,
+              updater: context.state.user
+            }
+          )
+        })
+      )
+
+      /**
+       * 3. Update target inventories (status: READY_TO_PUTAWAY => PUTTING_AWAY)
+       */
+      targetInventories = targetInventories.map((targetInventory: OrderInventory) => {
+        return {
+          ...targetInventory,
+          status: ORDER_PRODUCT_STATUS.PUTTING_AWAY,
+          updater: context.state.user
+        }
+      })
+      await trxMgr.getRepository(OrderInventory).save(targetInventories)
+
+      /**
+       * 4. Update putaway Worksheet (status: DEACTIVATED => EXECUTING)
+       */
+      const worksheet: Worksheet = await trxMgr.getRepository(Worksheet).save({
+        ...foundWorksheet,
+        status: WORKSHEET_STATUS.EXECUTING,
+        startedAt: new Date(),
+        updater: context.state.user
+      })
+
+      /**
+       * 5. Update Arrival Notice (status: READY_TO_PUTAWAY => PUTTING_AWAY)
+       */
+      const arrivalNotice: ArrivalNotice = foundWorksheet.arrivalNotice
+      await trxMgr.getRepository(ArrivalNotice).save({
+        ...arrivalNotice,
+        status: ORDER_STATUS.PUTTING_AWAY,
+        updater: context.state.user
+      })
+
+      return worksheet
+    })
   }
 }
--- server/graphql/resolvers/worksheet/complete-unloading.ts
+++ server/graphql/resolvers/worksheet/complete-unloading.ts
@@ -8,177 +8,190 @@ import {
   ORDER_STATUS
 } from '@things-factory/sales-base'
 import { Inventory } from '@things-factory/warehouse-base'
-import { Equal, getManager, In, Not } from 'typeorm'
+import { Equal, getManager, getRepository, EntityManager, In, Not, Any } from 'typeorm'
 import { WORKSHEET_STATUS, WORKSHEET_TYPE } from '../../../constants'
 import { Worksheet, WorksheetDetail } from '../../../entities'
 import { WorksheetNoGenerator } from '../../../utils/worksheet-no-generator'
-import { services } from './activate-putaway'
+import { activatePutaway } from './activate-putaway'
 
 export const completeUnloading = {
   async completeUnloading(_: any, { arrivalNoticeNo, worksheetDetails }, context: any) {
-    return await getManager().transaction(async trxMgr => {
-      /**
-       * 1. Validation for worksheet
-       *    - data existing
-       */
-      const arrivalNotice: ArrivalNotice = await trxMgr.getRepository(ArrivalNotice).findOne({
-        where: { domain: context.state.domain, name: arrivalNoticeNo, status: ORDER_STATUS.PROCESSING },
-        relations: ['bizplace']
-      })
-
-      if (!arrivalNotice) throw new Error(`ArrivalNotice doesn't exists.`)
-      const customerBizplace: Bizplace = arrivalNotice.bizplace
-      const foundWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
-        where: {
-          domain: context.state.domain,
-          bizplace: customerBizplace,
-          status: WORKSHEET_STATUS.EXECUTING,
-          type: WORKSHEET_TYPE.UNLOADING,
-          arrivalNotice
-        },
-        relations: ['bufferLocation', 'worksheetDetails', 'worksheetDetails.targetProduct']
-      })
-
-      if (!foundWorksheet) throw new Error(`Worksheet doesn't exists.`)
-      let foundWorksheetDetails: WorksheetDetail[] = foundWorksheet.worksheetDetails
-      let targetProducts: OrderProduct[] = foundWorksheetDetails.map(
-        (foundWSD: WorksheetDetail) => foundWSD.targetProduct
-      )
-
-      /**
-       * 3. Update worksheet detail status (EXECUTING => DONE) & issue note
-       */
-      foundWorksheetDetails = foundWorksheetDetails.map((foundWSD: WorksheetDetail) => {
-        const worksheetDetail: WorksheetDetail = worksheetDetails.find(
-          (worksheetDetail: WorksheetDetail) => foundWSD.name === worksheetDetail.name
-        )
-        if (worksheetDetail && worksheetDetail.issue) foundWSD.issue = worksheetDetail.issue
-        return {
-          ...foundWSD,
-          status: WORKSHEET_STATUS.DONE,
-          updater: context.state.user
-        }
-      })
-      await trxMgr.getRepository(WorksheetDetail).save(foundWorksheetDetails)
-
-      /**
-       * 4. Update worksheet status (status: EXECUTING => DONE)
-       */
-      await trxMgr.getRepository(Worksheet).save({
-        ...foundWorksheet,
-        status: WORKSHEET_STATUS.DONE,
-        endedAt: new Date(),
-        updater: context.state.user
-      })
-
-      /**
-       * 5. Update target products status (UNLOADED => TERMINATED)
-       */
-      targetProducts = targetProducts.map((targetProduct: OrderProduct) => {
-        return {
-          ...targetProduct,
-          status: ORDER_PRODUCT_STATUS.TERMINATED,
-          updater: context.state.user
-        }
-      })
-      await trxMgr.getRepository(OrderProduct).save(targetProducts)
-
-      /**
-       * 6. Check whether every related worksheet is completed
-       *    - if yes => Update Status of arrival notice
-       *    - VAS doesn't affect to status of arrival notice
-       */
-      const relatedWorksheets: Worksheet[] = await trxMgr.getRepository(Worksheet).find({
-        where: {
-          domain: context.state.domain,
-          bizplace: customerBizplace,
-          status: Not(Equal(WORKSHEET_STATUS.DONE)),
-          type: Not(In([WORKSHEET_TYPE.VAS])),
-          arrivalNotice
-        }
-      })
-
-      if (relatedWorksheets.length === 0) {
-        await trxMgr.getRepository(ArrivalNotice).save({
-          ...arrivalNotice,
-          status: ORDER_STATUS.READY_TO_PUTAWAY,
-          updater: context.status.user
+    const updatedWorksheet: Worksheet | boolean = await getManager().transaction(
+      async (trxMgr: EntityManager): Promise<Worksheet> => {
+        /**
+         * 1. Validation for worksheet
+         *    - data existing
+         */
+        const arrivalNotice: ArrivalNotice = await trxMgr.getRepository(ArrivalNotice).findOne({
+          where: { domain: context.state.domain, name: arrivalNoticeNo, status: ORDER_STATUS.PROCESSING },
+          relations: ['bizplace']
         })
-      }
 
-      /**
-       * 7. Create putaway worksheet
-       */
-      const putawayWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).save({
-        domain: context.state.domain,
-        arrivalNotice: arrivalNotice,
-        bizplace: customerBizplace,
-        name: WorksheetNoGenerator.putaway(),
-        type: WORKSHEET_TYPE.PUTAWAY,
-        status: WORKSHEET_STATUS.DEACTIVATED,
-        bufferLocation: foundWorksheet.bufferLocation,
-        creator: context.state.user,
-        updater: context.state.user
-      })
+        if (!arrivalNotice) throw new Error(`ArrivalNotice doesn't exists.`)
+        const customerBizplace: Bizplace = arrivalNotice.bizplace
+        let foundWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
+          where: {
+            domain: context.state.domain,
+            bizplace: customerBizplace,
+            status: WORKSHEET_STATUS.EXECUTING,
+            type: WORKSHEET_TYPE.UNLOADING,
+            arrivalNotice
+          },
+          relations: ['bufferLocation', 'worksheetDetails', 'worksheetDetails.targetProduct']
+        })
 
-      await Promise.all(
-        foundWorksheetDetails.map(async (worksheetDetail: WorksheetDetail) => {
-          const inventories: Inventory[] = await trxMgr.getRepository(Inventory).find({
-            where: {
-              domain: context.state.domain,
-              bizplace: customerBizplace,
-              batchId: worksheetDetail.targetProduct.batchId,
-              location: foundWorksheet.bufferLocation
-            },
-            relations: ['product', 'warehouse', 'location']
+        if (!foundWorksheet) throw new Error(`Worksheet doesn't exists.`)
+        let foundWorksheetDetails: WorksheetDetail[] = foundWorksheet.worksheetDetails
+        let targetProducts: OrderProduct[] = foundWorksheetDetails.map(
+          (foundWSD: WorksheetDetail) => foundWSD.targetProduct
+        )
+
+        /**
+         * 3. Update worksheet detail status (EXECUTING => DONE) & issue note
+         */
+        foundWorksheetDetails = foundWorksheetDetails.map((foundWSD: WorksheetDetail) => {
+          const worksheetDetail: WorksheetDetail = worksheetDetails.find(
+            (worksheetDetail: WorksheetDetail) => foundWSD.name === worksheetDetail.name
+          )
+          if (worksheetDetail && worksheetDetail.issue) foundWSD.issue = worksheetDetail.issue
+          return {
+            ...foundWSD,
+            status: WORKSHEET_STATUS.DONE,
+            updater: context.state.user
+          }
+        })
+        await trxMgr.getRepository(WorksheetDetail).save(foundWorksheetDetails)
+
+        /**
+         * 4. Update worksheet status (status: EXECUTING => DONE)
+         */
+        foundWorksheet = await trxMgr.getRepository(Worksheet).save({
+          ...foundWorksheet,
+          status: WORKSHEET_STATUS.DONE,
+          endedAt: new Date(),
+          updater: context.state.user
+        })
+
+        /**
+         * 5. Update target products status (UNLOADED => TERMINATED)
+         */
+        targetProducts = targetProducts.map((targetProduct: OrderProduct) => {
+          return {
+            ...targetProduct,
+            status: ORDER_PRODUCT_STATUS.TERMINATED,
+            updater: context.state.user
+          }
+        })
+        await trxMgr.getRepository(OrderProduct).save(targetProducts)
+
+        /**
+         * 6. Check whether every related worksheet is completed
+         *    - if yes => Update Status of arrival notice
+         *    - VAS doesn't affect to status of arrival notice
+         */
+        const relatedWorksheets: Worksheet[] = await trxMgr.getRepository(Worksheet).find({
+          where: {
+            domain: context.state.domain,
+            bizplace: customerBizplace,
+            status: Not(Equal(WORKSHEET_STATUS.DONE)),
+            type: Not(In([WORKSHEET_TYPE.VAS])),
+            arrivalNotice
+          }
+        })
+
+        if (relatedWorksheets.length === 0) {
+          await trxMgr.getRepository(ArrivalNotice).save({
+            ...arrivalNotice,
+            status: ORDER_STATUS.READY_TO_PUTAWAY,
+            updater: context.status.user
           })
+        }
 
-          await Promise.all(
-            inventories.map(async (inventory: Inventory) => {
-              const targetInventory: OrderInventory = await trxMgr.getRepository(OrderInventory).save({
-                name: OrderNoGenerator.orderInventory(),
-                seq: inventory.lastSeq,
-                releaseQty: inventory.qty,
-                status: ORDER_PRODUCT_STATUS.UNLOADED,
-                arrivalNotice,
-                inventory,
-                creator: context.state.user,
-                updater: context.state.user
-              })
+        /**
+         * 7. Create putaway worksheet
+         */
+        const putawayWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).save({
+          domain: context.state.domain,
+          arrivalNotice: arrivalNotice,
+          bizplace: customerBizplace,
+          name: WorksheetNoGenerator.putaway(),
+          type: WORKSHEET_TYPE.PUTAWAY,
+          status: WORKSHEET_STATUS.DEACTIVATED,
+          bufferLocation: foundWorksheet.bufferLocation,
+          creator: context.state.user,
+          updater: context.state.user
+        })
 
-              await trxMgr.getRepository(WorksheetDetail).save({
+        await Promise.all(
+          foundWorksheetDetails.map(async (worksheetDetail: WorksheetDetail) => {
+            const inventories: Inventory[] = await trxMgr.getRepository(Inventory).find({
+              where: {
                 domain: context.state.domain,
                 bizplace: customerBizplace,
-                name: WorksheetNoGenerator.putawayDetail(),
-                type: WORKSHEET_TYPE.PUTAWAY,
-                worksheet: putawayWorksheet,
-                targetInventory,
-                fromLocation: foundWorksheet.bufferLocation,
-                status: WORKSHEET_STATUS.DEACTIVATED,
-                creator: context.state.user,
-                updater: context.state.user
-              })
+                batchId: worksheetDetail.targetProduct.batchId,
+                location: foundWorksheet.bufferLocation
+              },
+              relations: ['product', 'warehouse', 'location']
             })
-          )
-        })
-      )
 
-      /**
-       * 7.1) If there's no issue related with this worksheet => Activate putaway worksheet right directely
-       */
-      const issuedWorksheetDetails = worksheetDetails.filter(
-        (worksheetDetail: WorksheetDetail) => worksheetDetail.issue
-      )
-      if (issuedWorksheetDetails.length === 0) {
-        const foundPutawayWorksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
-          where: { id: putawayWorksheet.id },
-          relations: ['worksheetDetails']
-        })
+            await Promise.all(
+              inventories.map(async (inventory: Inventory) => {
+                const targetInventory: OrderInventory = await trxMgr.getRepository(OrderInventory).save({
+                  name: OrderNoGenerator.orderInventory(),
+                  seq: inventory.lastSeq,
+                  releaseQty: inventory.qty,
+                  status: ORDER_PRODUCT_STATUS.UNLOADED,
+                  arrivalNotice,
+                  inventory,
+                  creator: context.state.user,
+                  updater: context.state.user
+                })
 
-        await services.activatePutaway(foundPutawayWorksheet.name, foundPutawayWorksheet.worksheetDetails, context)
+                await trxMgr.getRepository(WorksheetDetail).save({
+                  domain: context.state.domain,
+                  bizplace: customerBizplace,
+                  name: WorksheetNoGenerator.putawayDetail(),
+                  type: WORKSHEET_TYPE.PUTAWAY,
+                  worksheet: putawayWorksheet,
+                  targetInventory,
+                  fromLocation: foundWorksheet.bufferLocation,
+                  status: WORKSHEET_STATUS.DEACTIVATED,
+                  creator: context.state.user,
+                  updater: context.state.user
+                })
+              })
+            )
+          })
+        )
+
+        return foundWorksheet
       }
+    )
 
-      return foundWorksheet
-    })
+    /**
+     * @description This function should be executed outside of above function, cause at inside of activatePutaway function, there is another trxMgr
+     * and the trxMgr tries to get putawayWorksheet which is newly created. But there will be no record cause transaction not commited yet at here.
+     *
+     * 7.1) If there's no issue related with this worksheet => Activate putaway worksheet right directely
+     */
+    const issuedWorksheetDetails = worksheetDetails.filter((worksheetDetail: WorksheetDetail) => worksheetDetail.issue)
+    if (issuedWorksheetDetails.length === 0) {
+      const foundPutawayWorksheet: Worksheet = await getRepository(Worksheet).findOne({
+        where: {
+          domain: context.state.domain,
+          bizplace: context.state.bizplace,
+          arrivalNotice: updatedWorksheet.arrivalNotice,
+          type: WORKSHEET_TYPE.PUTAWAY,
+          status: WORKSHEET_STATUS.DEACTIVATED
+        },
+        relations: ['worksheetDetails']
+      })
+
+      await activatePutaway.activatePutaway(
+        _,
+        { worksheetNo: foundPutawayWorksheet.name, putawayWorksheetDetails: foundPutawayWorksheet.worksheetDetails },
+        context
+      )
+    }
   }
 }
