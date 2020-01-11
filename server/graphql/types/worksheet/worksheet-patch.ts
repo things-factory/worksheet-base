ee866e36556b3b47dbb88f5984d00480fad80dfb
ChrisLim <chrislim@hatiolab.com>
2020-01-11 10:16:35 +0900

    Revert

--- server/graphql/types/worksheet/worksheet-patch.ts
+++ server/graphql/types/worksheet/worksheet-patch.ts
@@ -7,6 +7,8 @@ export const WorksheetPatch = gql`
     type: String
     worksheetDetails: [ObjectRef]
     status: String
+    truckNo: String
+    ownCollection: Boolean
     cuFlag: String
   }
 `
