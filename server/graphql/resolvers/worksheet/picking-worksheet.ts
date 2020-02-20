ae4168919cf01bad9819aedb367d389452bb7734
leemyunggyu <jaylee@hatiolab.com>
2020-02-20 17:44:56 +0900

    v2.2.1

--- server/graphql/resolvers/worksheet/picking-worksheet.ts
+++ server/graphql/resolvers/worksheet/picking-worksheet.ts
@@ -1,47 +1,53 @@
-import { ORDER_STATUS, ReleaseGood, OrderInventory } from '@things-factory/sales-base'
+import { OrderInventory, ORDER_STATUS, ReleaseGood } from '@things-factory/sales-base'
 import { Inventory } from '@things-factory/warehouse-base'
-import { getRepository } from 'typeorm'
+import { EntityManager, getManager, getRepository } from 'typeorm'
 import { WORKSHEET_STATUS, WORKSHEET_TYPE } from '../../../constants'
 import { Worksheet, WorksheetDetail } from '../../../entities'
 
 export const pickingWorksheetResolver = {
-  async pickingWorksheet(_: any, { releaseGoodNo }, context: any) {
-    const releaseGood: ReleaseGood = await getRepository(ReleaseGood).findOne({
-      where: { domain: context.state.domain, name: releaseGoodNo, status: ORDER_STATUS.PICKING },
-      relations: ['bizplace']
-    })
+  async pickingWorksheet(_: any, { releaseGoodNo, sortings }, context: any) {
+    return await getManager().transaction(async (trxMgr: EntityManager) => {
+      const releaseGood: ReleaseGood = await getRepository(ReleaseGood).findOne({
+        where: { domain: context.state.domain, name: releaseGoodNo, status: ORDER_STATUS.PICKING },
+        relations: ['bizplace']
+      })
 
-    if (!releaseGood) throw new Error(`Release good doesn't exists.`)
+      const worksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
+        where: {
+          domain: context.state.domain,
+          releaseGood,
+          bizplace: releaseGood.bizplace,
+          type: WORKSHEET_TYPE.PICKING,
+          status: WORKSHEET_STATUS.EXECUTING
+        },
+        relations: ['bizplace']
+      })
 
-    const worksheet: Worksheet = await getRepository(Worksheet).findOne({
-      where: {
-        domain: context.state.domain,
-        releaseGood,
-        bizplace: releaseGood.bizplace,
-        type: WORKSHEET_TYPE.PICKING,
-        status: WORKSHEET_STATUS.EXECUTING
-      },
-      relations: [
-        'bizplace',
-        'worksheetDetails',
-        'worksheetDetails.targetInventory',
-        'worksheetDetails.targetInventory.inventory',
-        'worksheetDetails.targetInventory.inventory.location',
-        'worksheetDetails.targetInventory.inventory.product'
-      ]
-    })
+      const order = sortings.reduce((obj: {}, sorting: { name: string; desc: boolean }) => {
+        return {
+          ...obj,
+          [sorting.name]: sorting.desc ? 'DESC' : 'ASC'
+        }
+      }, {})
 
-    return {
-      worksheetInfo: {
-        bizplaceName: releaseGood.bizplace.name,
-        startedAt: worksheet.startedAt,
-        refNo: releaseGood.refNo
-      },
-      worksheetDetailInfos: worksheet.worksheetDetails
-        .sort((a: WorksheetDetail, b: WorksheetDetail) =>
-          a.targetInventory.inventory.location.name > b.targetInventory.inventory.location.name ? 1 : -1
-        )
-        .map(async (pickingWSD: WorksheetDetail) => {
+      const worksheetDetails: WorksheetDetail[] = await trxMgr.getRepository(WorksheetDetail).find({
+        where: { worksheet },
+        relations: [
+          'targetInventory',
+          'targetInventory.inventory',
+          'targetInventory.inventory.location',
+          'targetInventory.inventory.product'
+        ],
+        order
+      })
+
+      return {
+        worksheetInfo: {
+          bizplaceName: releaseGood.bizplace.name,
+          startedAt: worksheet.startedAt,
+          refNo: releaseGood.refNo
+        },
+        worksheetDetailInfos: worksheetDetails.map(async (pickingWSD: WorksheetDetail) => {
           const targetInventory: OrderInventory = pickingWSD.targetInventory
           const inventory: Inventory = targetInventory.inventory
           return {
@@ -58,6 +64,7 @@ export const pickingWorksheetResolver = {
             location: inventory.location
           }
         })
-    }
+      }
+    })
   }
 }
