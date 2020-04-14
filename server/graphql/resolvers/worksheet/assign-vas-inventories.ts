3ee3159d633be30755582822ec1b752bcb667b9f
leemyunggyu <jaylee@hatiolab.com>
2020-04-14 11:00:59 +0900

    v2.4.0-beta.27

--- server/graphql/resolvers/worksheet/assign-vas-inventories.ts
+++ server/graphql/resolvers/worksheet/assign-vas-inventories.ts
@@ -1,6 +1,7 @@
-import { User } from '@things-factory/auth-base'
+import { Bizplace } from '@things-factory/biz-base'
 import { OrderNoGenerator, OrderVas } from '@things-factory/sales-base'
 import { Domain } from '@things-factory/shell'
+import { Inventory } from '@things-factory/warehouse-base'
 import { getManager } from 'typeorm'
 import { WorksheetDetail } from '../../../entities'
 import { WorksheetNoGenerator } from '../../../utils'
@@ -10,14 +11,24 @@ export const assignVasInventoriesResolver = {
     return await getManager().transaction(async trxMgr => {
       const worksheetDetail: WorksheetDetail = await trxMgr.getRepository(WorksheetDetail).findOne({
         where: { id: worksheetDetailId },
-        relations: ['worksheet', 'orderVas']
+        relations: [
+          'domain',
+          'bizplace',
+          'worksheet',
+          'targetVas',
+          'targetVas.targetProduct',
+          'targetVas.arrivalNotice',
+          'targetVas.releaseGood',
+          'targetVas.shippingOrder',
+          'targetVas.vasOrder',
+          'targetVas.vas'
+        ]
       })
-
-      const domain: Domain = context.state.domain
-      const user: User = context.state.user
 
       const orderVas: OrderVas = worksheetDetail.targetVas
       const prevOrderVasId: string = orderVas.id
+      const domain: Domain = context.state.domain
+      const bizplace: Bizplace = worksheetDetail.bizplace
 
       delete worksheetDetail.id
 
@@ -29,27 +40,32 @@ export const assignVasInventoriesResolver = {
         // Create new order vas
         targetVas = await trxMgr.getRepository(OrderVas).save({
           ...targetVas,
+          domain,
+          bizplace,
           name: OrderNoGenerator.orderVas(),
           qty,
-          creator: user,
-          updater: user
+          inventory: await trxMgr.getRepository(Inventory).findOne(invId),
+          creator: context.state.user,
+          updater: context.state.user
         })
 
         // Create new worksheet detail
         await trxMgr.getRepository(WorksheetDetail).save({
           ...worksheetDetail,
+          domain,
+          bizplace,
           name: WorksheetNoGenerator.vasDetail(),
           targetVas,
-          creator: user,
-          updater: user
+          creator: context.state.user,
+          updater: context.state.user
         })
       }
 
-      // Delete previous order vas
-      await trxMgr.getRepository(OrderVas).delete(prevOrderVasId)
-
       // Delete previous worksheet detail
       await trxMgr.getRepository(WorksheetDetail).delete(worksheetDetailId)
+
+      // Delete previous order vas
+      await trxMgr.getRepository(OrderVas).delete(prevOrderVasId)
     })
   }
 }
