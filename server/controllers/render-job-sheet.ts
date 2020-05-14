c4179e8a64a57eabf0f13405644668593f963a5c
horwengliang <horwengliang@gmail.com>
2020-05-15 00:14:59 +0800

    add render-job-sheet

--- server/controllers/render-job-sheet.ts
+++ server/controllers/render-job-sheet.ts
@@ -1,38 +1,43 @@
 import { Attachment, STORAGE } from '@things-factory/attachment-base'
-import { Bizplace, Partner, ContactPoint } from '@things-factory/biz-base'
+import { Bizplace, Partner } from '@things-factory/biz-base'
 import { config } from '@things-factory/env'
-import { DeliveryOrder, OrderInventory, ORDER_STATUS, ArrivalNotice } from '@things-factory/sales-base'
+import {
+  ArrivalNotice,
+  DeliveryOrder,
+  JobSheet,
+  OrderInventory,
+  OrderVas,
+  OrderProduct,
+  ORDER_INVENTORY_STATUS,
+  ORDER_TYPE
+} from '@things-factory/sales-base'
+import { Product } from '@things-factory/product-base'
 import { Domain } from '@things-factory/shell'
-import { Inventory, Pallet } from '@things-factory/warehouse-base'
+import { Inventory, InventoryHistory, INVENTORY_STATUS } from '@things-factory/warehouse-base'
 import FormData from 'form-data'
 import fetch from 'node-fetch'
-import { Equal, getRepository, In } from 'typeorm'
-import { TEMPLATE_TYPE, WORKSHEET_STATUS, WORKSHEET_TYPE } from '../constants'
-import { Worksheet, WorksheetDetail } from '../entities'
+import { getRepository, IsNull, Not } from 'typeorm'
+import { TEMPLATE_TYPE, WORKSHEET_TYPE } from '../constants'
+import { Worksheet } from '../entities'
 
 const REPORT_API_URL = config.get('reportApiUrl', 'http://localhost:8888/rest/report/show_html')
 
-export async function renderJobSheet({ domain: domainName, doNo }) {
+export async function renderJobSheet({ domain: domainName, ganNo }) {
   const domain: Domain = await getRepository(Domain).findOne({
     where: { subdomain: domainName }
   }) //.. find domain
-  const foundDO: DeliveryOrder = await getRepository(DeliveryOrder).findOne({
-    where: { domain, name: doNo },
-    relations: ['domain', 'bizplace', 'transportDriver', 'transportVehicle', 'releaseGood', 'creator', 'updater']
-  }) // .. find do from deliveryOrderId
 
-  const ownTransportFlag: Boolean = foundDO.ownCollection
+  // find GAN
+  const foundGAN: ArrivalNotice = await getRepository(ArrivalNotice).findOne({
+    where: { domain, name: ganNo },
+    relations: ['bizplace', 'jobSheet']
+  })
 
-  let foundCP: ContactPoint = null
-  if (foundDO?.contactPointRefId) {
-    foundCP = await getRepository(ContactPoint).findOne({
-      where: { domain, id: foundDO.contactPointRefId }
-    })
-  }
+  // find job sheet
+  const foundJS: JobSheet = foundGAN.jobSheet
 
-  const foundGAN: ReleaseGood = foundDO.releaseGood
-  const partnerBiz: Bizplace = foundDO.bizplace //customer bizplace
-  const ownRefNo = foundRO.refNo
+  // customer bizplace
+  const partnerBiz: Bizplace = foundGAN.bizplace
 
   // find domain bizplace name, address, brn
   const foundDomainBizId: Partner = await getRepository(Partner).findOne({
@@ -40,49 +45,19 @@ export async function renderJobSheet({ domain: domainName, doNo }) {
     relations: ['domainBizplace']
   })
 
+  // owner domain bizplace
   const foundDomainBiz: Bizplace = await getRepository(Bizplace).findOne({
     where: { id: foundDomainBizId.domainBizplace.id }
   })
 
-  const foundDomainCP: ContactPoint = await getRepository(ContactPoint).findOne({
-    where: { domain, bizplace: foundDomainBiz }
-  })
-
+  // find unloading worksheet for getting unloading time
   const foundWS: Worksheet = await getRepository(Worksheet).findOne({
-    where: { domain, releaseGood: foundRO },
+    where: { domain, arrivalNotice: foundGAN, type: WORKSHEET_TYPE.UNLOADING },
     relations: ['updater']
   })
 
-  //find reusable pallet
-  const foundRP: Pallet[] = await getRepository(Pallet).find({
-    where: { domain, refOrderNo: foundRO.name }
-  })
-
-  //find list of loaded inventory
-  const targetInventories: OrderInventory[] = await getRepository(OrderInventory).find({
-    where: { domain, deliveryOrder: foundDO },
-    relations: ['inventory']
-  })
-  const orderInvIds: string[] = targetInventories.map((oi: any) => oi.id)
-
-  const foundWSD: WorksheetDetail[] = await getRepository(WorksheetDetail).find({
-    where: {
-      domain,
-      targetInventory: In(orderInvIds),
-      type: WORKSHEET_TYPE.LOADING,
-      status: Equal(WORKSHEET_STATUS.DONE)
-    },
-    relations: [
-      'targetInventory',
-      'targetInventory.inventory',
-      'targetInventory.inventory.location',
-      'targetInventory.inventory.product',
-      'updater'
-    ]
-  })
-
   const foundTemplate: Attachment = await getRepository(Attachment).findOne({
-    where: { domain, category: TEMPLATE_TYPE.DO_TEMPLATE }
+    where: { domain, category: TEMPLATE_TYPE.JOB_TEMPLATE }
   })
 
   const foundLogo: Attachment = await getRepository(Attachment).findOne({
@@ -92,92 +67,90 @@ export async function renderJobSheet({ domain: domainName, doNo }) {
     }
   })
 
-  let foundDriver: any = null
-  if (foundDO.status !== ORDER_STATUS.READY_TO_DISPATCH) {
-    if (foundDO?.ownCollection && foundDO?.otherDriver) {
-      foundDriver = foundDO.otherDriver
-    } else {
-      foundDriver = foundDO.transportDriver.name
-    }
-  }
-
   const template = await STORAGE.readFile(foundTemplate.path, 'utf-8')
   let logo = null
   if (foundLogo?.path) {
     logo = 'data:' + foundLogo.mimetype + ';base64,' + (await STORAGE.readFile(foundLogo.path, 'base64'))
   }
 
-  const productList: any = foundWSD
-    .map((wsd: WorksheetDetail) => {
-      const targetInventory: OrderInventory = wsd.targetInventory
-      const inventory: Inventory = targetInventory.inventory
-      return {
-        product_name: `${inventory.product.name} (${inventory.product.description})`,
-        product_type: inventory.packingType,
-        product_batch: inventory.batchId,
-        product_qty: targetInventory.releaseQty,
-        product_weight: targetInventory.releaseWeight,
-        remark: targetInventory.remark
-      }
-    })
-    .reduce((newItem, item) => {
-      var foundItem = newItem.find(
-        newItem => newItem.product_name === item.product_name && newItem.product_batch === item.product_batch
-      )
-      if (!foundItem) {
-        foundItem = {
-          product_name: item.product_name,
-          product_type: item.product_type,
-          product_batch: item.product_batch,
-          product_qty: item.product_qty,
-          product_weight: item.product_weight,
-          remark: 1
-        }
+  // find list of unloaded product
+  const targetProducts: OrderProduct[] = await getRepository(OrderProduct).find({
+    where: { domain, arrivalNotice: foundGAN, actualPalletQty: Not(IsNull()) },
+    relations: ['product']
+  })
 
-        newItem.push(foundItem)
-        return newItem
-      } else {
-        return newItem.map(ni => {
-          if (ni.product_name === item.product_name && ni.product_batch === item.product_batch) {
-            return {
-              ...ni,
-              remark: ni.remark + 1,
-              product_qty: ni.product_qty + item.product_qty
-            }
-          } else {
-            return ni
-          }
-        })
-      }
-    }, [])
+  const products: Product[] = targetProducts.map((op: OrderProduct) => op.product)
+
+  // sum up unloaded pack and pallet qty
+  const sumPackQty: any = targetProducts.map((op: OrderProduct) => op.actualPackQty).reduce((a, b) => a + b, 0)
+  const sumPalletQty: any = targetProducts.map((op: OrderProduct) => op.actualPalletQty).reduce((a, b) => a + b, 0)
+
+  //find list of unloaded inventory
+  const targetInventories: OrderInventory[] = await getRepository(OrderInventory).find({
+    where: { domain, arrivalNotice: foundGAN },
+    relations: ['inventory', 'inventory.product', 'inventory.location']
+  })
+
+  const productList: any = targetInventories.map(async (oi: OrderInventory) => {
+    const inventory: Inventory = oi.inventory
+    const foundIH: InventoryHistory[] = await getRepository(InventoryHistory).find({
+      where: { domain, palletId: inventory.palletId, product: inventory.product.id }
+    })
+
+    const foundOV: OrderVas[] = await getRepository(OrderVas).find({
+      where: { domain, inventory },
+      relations: ['vas']
+    })
+
+    let terminatedInv = null
+    if (inventory.status === INVENTORY_STATUS.TERMINATED) {
+      terminatedInv = foundIH.filter(
+        (ih: InventoryHistory) =>
+          ih.status == ORDER_INVENTORY_STATUS.TERMINATED && ih.transactionType === ORDER_INVENTORY_STATUS.TERMINATED
+      )
+    }
+
+    const foundOI: OrderInventory[] = await getRepository(OrderInventory).find({
+      where: { domain, inventory, type: ORDER_TYPE.RELEASE_OF_GOODS },
+      relations: ['deliveryOrders']
+    })
+
+    const foundDO: DeliveryOrder[] = foundOI.map((oi: OrderInventory) => oi.deliveryOrder)
+
+    return {
+      pallet_id: inventory.palletId,
+      product_name: inventory.product.name,
+      product_type: inventory.packingType,
+      product_batch: inventory.batchId,
+      in_pallet: inventory.createdAt,
+      out_pallet: terminatedInv ? terminatedInv.updatedAt : '',
+      do_list: foundDO.map(dos => dos.name).join(', '),
+      product_qty: oi.releaseQty,
+      remark: foundOV.map(ov => ov.vas.name).join(', ')
+    }
+  })
 
   const data = {
     logo_url: logo,
     customer_biz: partnerBiz.name,
-    delivery_company: foundCP ? foundCP.name : null,
     company_domain: foundDomainBiz.name,
     company_brn: foundDomainBiz.description,
     company_address: foundDomainBiz.address,
-    company_phone: foundDomainCP.phone,
-    company_email: foundDomainCP.email,
-    own_collection: ownTransportFlag ? '[SELF-COLLECTION]' : `[${domain.brandName} TRANSPORT]`,
-    destination: foundDO.to || '',
-    ref_no: ownRefNo ? `${foundRO.name} / ${foundRO.refNo}` : `${foundRO.name}`,
-    order_no: foundDO.name,
-    delivery_date: foundDO.deliveryDate || '',
-    truck_no: foundDO.truckNo,
-    driver_name: foundDriver || '',
-    pallet_qty: foundDO.palletQty,
-    worker_name: foundWS.updater.name,
-    pallet_list: foundRP.map(rp => rp.name).join(', '),
-    product_list: productList.map((prod: any, idx) => {
-      return {
-        ...prod,
-        list_no: idx + 1,
-        remark: prod.remark > 1 ? `${prod.remark} PALLETS` : `${prod.remark} PALLET`
-      }
-    })
-  } //.. make data from do
+    container_no: foundGAN.containerNo,
+    container_size: foundJS.containerSize,
+    eta: foundGAN.eta,
+    ata: foundGAN.ata,
+    unloading_date: foundWS.startedAt,
+    mt_date: foundJS.containerMtDate,
+    advise_mt_date: foundJS.adviseMtDate,
+    loose_item: foundGAN.looseItem ? 'Y' : 'N',
+    no_of_pallet: foundGAN.looseItem ? `${sumPackQty} CTN` : `${sumPalletQty} PALLETS`,
+    commodity: products.map(prod => prod.name).join(', '),
+    created_on: foundJS.createdAt,
+    job_no: foundJS.name,
+    ref_no: foundGAN.name,
+    product_list: productList
+  }
   const formData = new FormData()
 
   formData.append('template', template)
