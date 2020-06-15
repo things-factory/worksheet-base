d0c374981ee4b12bddbb39371176fff4e757da0e
farishelmi <farishelmi@hatiolab.com>
2020-06-15 13:38:49 +0800

    fixed undo picking assignment
    previously, the lockedQty and lockedWeight did not deducted when undo picking assignment

--- server/graphql/resolvers/worksheet/undo-picking-assignment.ts
+++ server/graphql/resolvers/worksheet/undo-picking-assignment.ts
@@ -1,4 +1,5 @@
 import { OrderInventory } from '@things-factory/sales-base'
+import { Inventory } from '@things-factory/warehouse-base'
 import { EntityManager, getManager } from 'typeorm'
 import { Worksheet, WorksheetDetail } from '../../../entities'
 
@@ -7,7 +8,7 @@ export const undoPickingAssigmentResolver = {
     return await getManager().transaction(async (trxMgr: EntityManager) => {
       const worksheet: Worksheet = await trxMgr.getRepository(Worksheet).findOne({
         where: { name: worksheetNo, domain: context.state.domain },
-        relations: ['bizplace', 'releaseGood', 'worksheetDetails', 'worksheetDetails.targetInventory']
+        relations: ['bizplace', 'releaseGood', 'worksheetDetails', 'worksheetDetails.targetInventory', 'worksheetDetails.targetInventory.inventory']
       })
       const worksheetDetails: WorksheetDetail[] = worksheet.worksheetDetails.filter(
         (wsd: WorksheetDetail) =>
@@ -18,6 +19,15 @@ export const undoPickingAssigmentResolver = {
 
       const wsdIds: string[] = worksheetDetails.map((wsd: WorksheetDetail) => wsd.id)
       const orderInvIds: string[] = worksheetDetails.map((wsd: WorksheetDetail) => wsd.targetInventory.id)
+
+      worksheetDetails.map( async (wsd: WorksheetDetail) => {
+        let inv = wsd.targetInventory?.inventory
+        await trxMgr.getRepository(Inventory).save({
+          ...inv,
+          lockedQty: inv.lockedQty - wsd.targetInventory?.releaseQty,
+          lockedWeight: inv.lockedWeight - wsd.targetInventory?.releaseWeight
+        })
+      })
 
       await trxMgr.getRepository(WorksheetDetail).delete(wsdIds)
       await trxMgr.getRepository(OrderInventory).delete(orderInvIds)
