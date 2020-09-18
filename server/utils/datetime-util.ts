f52009c33c17028aef87918028df5f8b86d003f2
Joe Kin <joekin95@gmail.com>
2020-09-18 18:07:00 +0800

    amend job sheet to get timezone from client side

--- server/utils/datetime-util.ts
+++ server/utils/datetime-util.ts
@@ -16,11 +16,11 @@ export class DateTimeConverter {
     return unloadDate
   }
 
-  static datetime(dateTime) {
+  static datetime(dateTime, timezone) {
     let unloadDate = ''
     if (dateTime) {
       const datetime = Number(dateTime)
-      const timezoneOffset = new Date(datetime).getTimezoneOffset() * 60000
+      const timezoneOffset = timezone * 60000
       const newUnloadDate = new Date(datetime - timezoneOffset).toISOString().slice(0, -1)
 
       var dateTimeParts: any = newUnloadDate.split('T')
