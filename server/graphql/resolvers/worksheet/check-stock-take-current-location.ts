51fc011c8491b1f4c514bc3ed9f012ec005ab807
horwengliang <horwengliang@gmail.com>
2020-11-25 09:40:12 +0800

    [cycle-count] fixed check unknown inventory

--- server/graphql/resolvers/worksheet/check-stock-take-current-location.ts
+++ server/graphql/resolvers/worksheet/check-stock-take-current-location.ts
@@ -1,10 +1,13 @@
 import { InventoryCheck, OrderInventory } from '@things-factory/sales-base'
 import { Inventory } from '@things-factory/warehouse-base'
 import { getRepository, Repository } from 'typeorm'
+import { WORKSHEET_STATUS } from '../../../constants'
+import { Worksheet, WorksheetDetail } from '../../../entities'
 
 export const checkStockTakeCurrentLocationResolver = {
   async checkStockTakeCurrentLocation(_: any, { palletId, cycleCountNo }, context: any) {
     const invRepo: Repository<Inventory> = getRepository(Inventory)
+    const worksheetDetailRepo: Repository<WorksheetDetail> = getRepository(WorksheetDetail)
     const inventoryCheckRepo: Repository<InventoryCheck> = getRepository(InventoryCheck)
 
     const inventory: Inventory = await invRepo.findOne({
@@ -28,9 +31,22 @@ export const checkStockTakeCurrentLocationResolver = {
       (oi: OrderInventory) => oi.inventory.palletId === inventory.palletId
     ).inventory
 
+    const foundWSD: any[] = await Promise.all(
+      orderInventories
+        .filter((oi: OrderInventory) => oi.inventory.palletId === inventory.palletId)
+        .map(async (oi: OrderInventory) => {
+          const matchingWSD = await worksheetDetailRepo.findOne({
+            where: { domain: context.state.domain, targetInventory: oi }
+          })
+
+          return matchingWSD
+        })
+    )
+
     if (!foundInventory) throw new Error(`${inventory.palletId} is not in cycle count worksheet`)
 
     return {
+      worksheetDetailName: foundWSD[0].name,
       currentLocation: foundInventory.location.name,
       palletId: foundInventory.palletId,
       batchId: foundInventory.batchId,
--- server/graphql/types/worksheet/cycle-count-worksheet.ts
+++ server/graphql/types/worksheet/cycle-count-worksheet.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server-koa'
 export const CycleCountWorksheet = gql`
   type CycleCountWorksheet {
     name: String
+    worksheetDetailName: String
     palletId: String
     currentLocation: String
     batchId: String
